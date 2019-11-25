60598bad5289d5552712ce42d52f9a156ff9df83
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-11-25 16:53:57 +0530

    Add:object reflector

--- src/test/java/com/bridgeLbaz/RealMoodAnalyserTest.java
+++ src/test/java/com/bridgeLbaz/RealMoodAnalyserTest.java
@@ -5,43 +5,49 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class RealMoodAnalyserTest {
+//    @Test
+//    public void givenMessage_WhenSad_ShowReturnSad(){
+//        RealMoodAnalyser moodAnalyser = new RealMoodAnalyser();
+//        String mood = moodAnalyser.analyseMood("This is Sad Mood");
+//        Assert.assertEquals("SAD",mood);
+//    }
+//
+//    @Test
+//    public void givenMessage_WhenHappy_ShowReturnHappy() {
+//        RealMoodAnalyser moodAnalyser = new RealMoodAnalyser();
+//        String mood = moodAnalyser.analyseMood("This is Happy Mood");
+//        Assert.assertEquals("HAPPY",mood);
+//    }
+//
+//    @Test
+//    public void nullPointerException() {
+//        RealMoodAnalyser moodAnalyser = new RealMoodAnalyser();
+//        String mood = moodAnalyser.analyseMood(null);
+//        Assert.assertEquals("HAPPY",mood);
+//    }
+//
+//    @Test
+//    public void givenMessage_whenProper_ReturnHappy() throws MoodAnalysiseException {
+//        RealMoodAnalyser moodAnalyser = new RealMoodAnalyser();
+//        ExpectedException exceptionRule = ExpectedException.none();
+//        exceptionRule.expect(MoodAnalysiseException.class);
+//        moodAnalyser.analyseMood(null);
+//    }
+
+//    @Test
+//    public void givenMoodAnlayserClass_whenProper_ReturnObject() {
+//        RealMoodAnalyser realMoodAnalyser = MoodAnalyserFactory.createMoodAnalyser("I an in Happy mood");
+//        try{
+//            String mood = realMoodAnalyser.analyseMood();
+//            Assert.assertEquals("HAPPY",mood);
+//        }catch (MoodAnalysiseException e){
+//            e.printStackTrace();
+//        }
+//    }
     @Test
-    public void givenMessage_WhenSad_ShowReturnSad(){
-        RealMoodAnalyser moodAnalyser = new RealMoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is Sad Mood");
-        Assert.assertEquals("SAD",mood);
+    public void givenMessage_WhenProper_Return_Object_Reflector() {
+        RealMoodAnalyser moodAnalyzer = new RealMoodAnalyser("I an in Happy mood");
+        ObjectReflector.dump(moodAnalyzer, 4);
     }
 
-    @Test
-    public void givenMessage_WhenHappy_ShowReturnHappy() {
-        RealMoodAnalyser moodAnalyser = new RealMoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is Happy Mood");
-        Assert.assertEquals("HAPPY",mood);
-    }
-
-    @Test
-    public void nullPointerException() {
-        RealMoodAnalyser moodAnalyser = new RealMoodAnalyser();
-        String mood = moodAnalyser.analyseMood(null);
-        Assert.assertEquals("HAPPY",mood);
-    }
-
-    @Test
-    public void givenMessage_whenProper_ReturnHappy() throws MoodAnalysiseException {
-        RealMoodAnalyser moodAnalyser = new RealMoodAnalyser();
-        ExpectedException exceptionRule = ExpectedException.none();
-        exceptionRule.expect(MoodAnalysiseException.class);
-        moodAnalyser.analyseMood(null);
-    }
-
-    @Test
-    public void givenMoodAnlayserClass_whenProper_ReturnObject() {
-        RealMoodAnalyser realMoodAnalyser = MoodAnalyserFactory.createMoodAnalyser("I an in Happy mood");
-        try{
-            String mood = realMoodAnalyser.analyseMood();
-            Assert.assertEquals("HAPPY",mood);
-        }catch (MoodAnalysiseException e){
-            e.printStackTrace();
-        }
-    }
 }
